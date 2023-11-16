32ee57ef3234391bd0d9cf5a77bb3f457b8cb65d
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-11-16 14:56:33 -0500

    Created functionality for Stack

--- stack.js
+++ stack.js
@@ -20,26 +20,46 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
+    let newNode = new Node(val)
 
+    if (this.size === 0){
+      this.first = newNode
+      this.last = newNode
+    } else {
+      newNode.next = this.first
+      this.first = newNode
+    }
+    this.size += 1
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
+    if (this.size === 0) throw Error("The stack is currently empty")
 
+    let popped = this.first
+
+    this.first = this.first.next
+
+    this.size -= 1
+    return popped.val
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
+    if (this.size === 0) throw Error("The stack is currently empty")
 
+    return this.first.val
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
+    if (this.size === 0) return true
 
+    return false
   }
 }
 
