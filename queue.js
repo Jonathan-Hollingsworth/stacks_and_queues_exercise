3beff263fbb65c0403a833737655a60375fe7684
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-11-16 14:48:33 -0500

    Created functionality for Queue

--- queue.js
+++ queue.js
@@ -20,26 +20,46 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    let newNode = new Node(val)
 
+    if (this.size === 0){
+      this.first = newNode
+      this.last = newNode
+    } else {
+      this.last.next = newNode
+      this.last = newNode
+    }
+    this.size += 1
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.size === 0) throw Error("The queue is currently empty")
 
+    let dequeued = this.first
+
+    this.first = this.first.next
+
+    this.size -= 1
+    return dequeued.val
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
+    if (this.size === 0) throw Error("The queue is currently empty")
 
+    return this.first.val
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
+    if (this.size === 0) return true
 
+    return false
   }
 }
 
